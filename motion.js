6b5f754866ac7b83bdcc617a7adc82028107fbdb
Leonardo McNaughton <9lmcnaughton@stpatrick.org>
2017-05-17 11:30:36 -0500

    Add files via upload

--- motion.js
+++ motion.js
@@ -8,24 +8,24 @@ exports.distance = function(v,t){
 exports.distance = function(a,v,t){
   let d = 0.5 * a * t * t + v * t;
   return d;
-}
+};
 exports.velocity = function(d,t){
   let v = d/t;
   return v;
-}
-exports.velocity = function(){
+};
+exports.velocity = function(d,a,t){
   let v = (d-0.5*a*t*t)/t;
   return v;
-}
+};
 exports.time = function(d,v){
   let t = d/v;
   return t;
-}
-exports.time = function(){
+};
+exports.time = function(Vf,Vi,a){
   let t = (Vf-Vi)/a;
   return t;
-}
-exports.acceleration = function(){
-  let a = ;
+};
+exports.acceleration = function(Vf,Vi,t){
+  let a = (Vf-Vi)/t;
   return a;
-}
+};
