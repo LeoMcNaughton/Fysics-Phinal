1fed3cecbbcbb408833fadc1576fc0e55ecd2e90
Leonardo McNaughton <9lmcnaughton@stpatrick.org>
2017-05-18 10:55:26 -0500

    Add files via upload

--- energy.js
+++ energy.js
@@ -2,14 +2,14 @@ var exports = module.exports = {};
 
 //use this prototype to create the other functions
 exports.kineticEnergy = function(m,v){
-  let ke = 0.5*m*v*;
+  let ke = 0.5*m*v*v;
   return ke;
 };
 exports.mass = function(){
   let m = (2*k)/v*v;
   return m;
 };
-eports.velocity = function(){
+exports.velocity = function(){
   let v = Math.sqrt((2*k)/m)
   return v;
-};s
+};
--- physics.js
+++ physics.js
@@ -2,15 +2,66 @@ const motion = require('./motion');
 const energy = require('./energy');
 const readline = require('readline');
 
+function end(){
+    terminal.close();
+}
 
-const terminal = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
-});
+function printOptions(){
+    console.log("\nEnter the variables you know in a comma list.");
+    console.log("If you do not know a variable, then type n");
+    console.log("distance,velocity,acceleration,time,mass,energy");
+}
+
+function convertToArray(strcma){
+    strcma = strcma.split(',');
+    for(let i=0;i<strcma.length;i++){
+        strcma[i] = Number(strcma[i]);
+    }
+    return strcma;
+}
+
+// terminal.question("My question?", function(str){
+     //If I typed in "Motion"
+//     str.toLowerCase();
+     //Now str == "motion";
+// });
+
+function getinput(){
+
+  const terminal = readline.createInterface({
+      input:process.stdin,
+      output:process.stdout
+  });
 
 terminal.question("What type of equation are you looking for?",function(str){
 
     //you will configure your series of questions inside here.
+    if(str == "motion"){
+    terminal.question("What are you looking for?",function(str){
+      str = str.toLowerCase();
+      if(str == "d"){
+        printOptions();
+      }
+      else if(str == "v"){
+        printOptions();
+      }
+      else if(str == "t"){
+        printOptions();
+      }
+      else if(str == "a"){
+       printOptions();
+      }
+    });
+  }
+ });
+    else if(str == "energy"){
+      terminal.question("What are you looking for?",function(str){
+        str = str.toLowerCase();
+        if(str == "ke"){
+          printOptions();
+        }
+      });
+    }
+}
 
-
-});
+getinput();
